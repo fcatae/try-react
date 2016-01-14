e658cadfced82123923e16d7587412907531c114
Fabricio Catae <fabricio.catae@microsoft.com>
2016-01-14 01:20:14 -0200

    mustache

--- poke.js
+++ poke.js
@@ -5,14 +5,11 @@ $.get('poke.json', function(data) {
    var lista = document.getElementById('lista');
    
    data.pokemon.map(function(pokemon) {
-       console.log('a')
        var elem = document.createElement('li');
        
-       var link = document.createElement('a');
-       link.setAttribute('href','http://pokeapi.co/media/img/9.png');
-       link.textContent = pokemon.name;
-       
-       elem.appendChild(link);
+       var view = { link: 'http://pokeapi.co/media/img/9.png', text: pokemon.name };
+       var output = Mustache.render('<a href="{{link}}">{{text}}</a>', view);
+       elem.innerHTML = output;       
        
        lista.appendChild(elem);
    })
@@ -20,6 +17,5 @@ $.get('poke.json', function(data) {
 });
 
 // $.get('http://pokeapi.co/api/v1/sprite/1/')
-$.get('http://pokeapi.co/media/img/9.png', function(data) {
-    
-})
+// $.get('http://pokeapi.co/media/img/9.png', function(data) {})
+
